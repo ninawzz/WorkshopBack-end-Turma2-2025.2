cd877e4f16e062d93021b1303961d68eb798db98
ninawzz <nina.troccoli@live.com>
2025-09-08 15:25:11 -0300

    adição de arquivos de exercícios com correções de erros e exemplos de uso

---
+++ projeto/dia3/atv1-facil.py
@@ -0,0 +1,7 @@
+# print("Olá, mundo!"
+print("Olá, mundo!")
+
+      
+#     ERRO:
+# Syntax error
+# O erro foi causado porque faltou um ')' para fechar o print
---
+++ projeto/dia3/atv3-medio.py
@@ -0,0 +1,16 @@
+#Codigo errado:
+# def somar(a, b):
+#     return a + b
+
+# resultado = somar(10, "5")
+# print(resultado)
+
+# Codigo certo:
+def somar(a, b):
+    return a + b
+
+resultado = somar(10, 5)
+print(resultado)
+
+# ERRO -> TypeError
+# O erro foi causado porque não é possivel somar int com string
